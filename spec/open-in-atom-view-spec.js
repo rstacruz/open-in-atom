12fa53346ae9109cade612d422493889a585795f
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-07-20 14:17:14 +0800

    Initial commit

---
+++ spec/open-in-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import OpenInAtomView from '../lib/open-in-atom-view';
+
+describe('OpenInAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
